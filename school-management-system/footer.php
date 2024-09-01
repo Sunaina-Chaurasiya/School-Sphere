832f8a810fbbe8ed14bc05234448c172e06cec16
Sunaina-Chaurasiya <sunainamonika2703@gmail.com>
2024-09-01 16:20:45 +0530

    footer.php

--- school-management-system/footer.php
+++ school-management-system/footer.php
@@ -3,8 +3,8 @@
 <body>
     <footer>
         <div class="footer-container">
-            <p>&copy; 2024 School Management System. All rights reserved.</p>
-            <p>Contact: <a href="mailto:info@schoolmanagement.com">info@schoolmanagement.com</a></p>
+            <p>&copy; 2024 School Sphere. All rights reserved.</p>
+            <p>Contact: <a href="mailto:info@schoolsphere.com">info@schoolsphere.com</a></p>
         </div>
     </footer>
 </body>
